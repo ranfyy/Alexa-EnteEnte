1aef9e860f68cad20ade35b0233eff5cded7828f
ranfyy <ranfyy@users.noreply.github.com>
2017-06-10 12:56:28 +0200

    release version 1.0

--- index.py
+++ index.py
@@ -7,9 +7,10 @@ import re
 s3_bucket  = "https://s3-eu-west-1.amazonaws.com/alexa-enteente/"
 img_small  = s3_bucket + "sketch-756.png"
 img_large  = s3_bucket + "sketch-1280.png"
-myname     = 'Quack-Quack'
+myname     = 'Nak-Nak-Nak'
 lang       = ''
 
+br = "\n\n"
 
 def lambda_handler(event, context):
 
@@ -39,7 +40,7 @@ def lambda_handler(event, context):
                            event['session'])
 
     if event['request']['type'] == "LaunchRequest":
-        return on_launch(event['request'], event['session'])
+        return on_launch(event['request'], event['session'], lang)
     elif event['request']['type'] == "IntentRequest":
         return on_intent(event['request'], event['session'], lang)
     elif event['request']['type'] == "SessionEndedRequest":
@@ -53,7 +54,7 @@ def on_session_started(session_started_request, session):
           + ", sessionId=" + session['sessionId'])
 
 
-def on_launch(launch_request, session):
+def on_launch(launch_request, session, lang):
     """ Called when the user launches the skill without specifying what they
     want
     """
@@ -98,32 +99,37 @@ def on_session_ended(session_ended_request, session):
 
 def get_welcome_response(lang):
     session_attributes = {}
+    myname = 'Nak-Nak-Nak'
     if lang == 'de':
-        speech_output = "Ich kann deine Frage an DackDackGo richten, eine freie Suchmaschine. " \
-                    "Stell mir eine Frage, und ich liefere dir Suchergebnisse und Informationen."
-        reprompt_text = "Du kannst mich alles fragen wie zum Beispiel: Was ist Python?"
-        myname = 'Ente-Ente'
+        speech_output = "Ich kann deine Frage an Dack Dack Go richten, eine freie Suchmaschine. " \
+                    + br + "Sie sammelt oder teilt keine personenbezogenen Daten. " \
+                    + br + br + "Stell mir eine Frage, und ich liefere dir Suchergebnisse und Informationen."
+        reprompt_text = "Du kannst mich alles fragen wie zum Beispiel: Wer sind die Simpsons?"
+        card_text = speech_output.replace( "Dack Dack Go", "DuckDuckGo")
     elif lang == 'en':
         speech_output = "I can look up your questions on Duck Duck Go, a free search engine. " \
-                    "Give me something to look up, and I'll return search results and info."
+                    + br + "It does not collect or share personal information. " \
+                    + br + br + "Give me something to look up, and I'll return search results and info."
         reprompt_text = "You can ask me something like, What is Python?"
         myname = 'Quack-Quack'
+        card_text = speech_output.replace( "Duck Duck Go", "DuckDuckGo")
     card_title = myname
 
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
-        card_title, speech_output, reprompt_text, should_end_session, speech_output, None))
+        card_title, speech_output, reprompt_text, should_end_session, card_text, None))
 
 def get_hello_response(lang):
     session_attributes = {}
+    speech_output = 'Ask me'
+    reprompt_text = 'reprompt'
     if lang == 'de':
-        speech_output = "Frag mich was!"
-        reprompt_text = "Du kannst mich alles fragen wie zum Beispiel: Was ist Python?"
-        myname = "Ente Ente"
+        speech_output = 'Willkommen bei Naak-Naak-Naak. Frag mich irgendwas!'
+        reprompt_text = 'Du kannst mich alles fragen wie zum Beispiel: Wer sind die Simpsons?'
     elif lang == 'en':
-        speech_output = "Ask me something!"
-        reprompt_text = "You can ask me something like, What is Python?"
-    card_title = myname
+        speech_output = 'Welcome to Quack-Quack. Ask me something!'
+        reprompt_text = 'You can ask me something like, What is Python?'
+    card_title = None
 
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
@@ -135,49 +141,102 @@ def handle_session_end_request():
     return build_response({}, build_speechlet_response(
         None, None, None, should_end_session, None, None))
 
-def searchDuck(intent, session, lang):
-    card_img = None
-    if 'query' in intent['slots']:
-        lookupString = intent['slots']['query']['value']
+
+def format_url(s, lang='en', all=False):
+    if not s:
+        ret = ''
     else:
-        speech_output = "I'm sorry, I didn't understand, you can say something like 'Who is Tycho the musician?'"
+        if all:
+            s = s.replace('https://', '')
+            s = s.replace('http://',  '')
+            src = 'forward u r l : '
+            slash = ' slash '
+            if lang == 'de':
+                src = 'Weiterleitungs-U R L : '
+                slash = ' Släsch '
+            s = s.replace('/',  slash)
+        else:
+            s = re.split('/', s)
+            s = s[2]
+            src = 'source: '
+            if lang == 'de':
+                src = 'Quelle: '
+        dot = ' dot '
         if lang == 'de':
-            speech_output = "Sorry, das habe ich nicht verstanden"
+            dot = ' Punkt '
+        s = s.replace("www.", "")
+        s = s.replace(".", dot)
+        s = s.replace(" de", " d.e.")
+        s = s.replace("de ", "d.e. ")
+        s = s.replace(" uk", " u.k.")
+        s = s.replace("uk ", "u.k. ")
+        s = s.replace(" fr", " f.r.")
+        s = s.replace(" io", " i.o.")
+        ret = br + src + s
+    return ret
+
+def searchDuck(intent, session, lang='en'):
+    card_img = None
+    card_text = None
+    card_title = None
+    lookupString = ''
+    reprompt_text = ""
+    should_end_session = True
+    speech_output = ''
+    if 'query' in intent['slots']:
+        if 'value' in intent['slots']['query']:
+            lookupString = intent['slots']['query']['value']
+    else:
+        speech_output = 'Sorry, I didn\'t understand'
+        if lang == 'de':
+            speech_output = 'Sorry, das habe ich nicht verstanden'
         elif lang == 'en':
-            speech_output = "I'm sorry, I didn't understand, you can say something like 'Who is Tycho the musician?'"
-        card_title = None
-        should_end_session = True
-        reprompt_text = ""
+            speech_output = 'I\'m sorry, I didn\'t understand, you can say something like: Who is Tycho the musician?'
+
         return build_response({}, build_speechlet_response(
             card_title, speech_output, reprompt_text, should_end_session, None, None))
 
     #Gets the First Result of a DuckDuckGo
     try:
-        queryRun, card_img = duckduckgo.get_zci(lookupString, lang) 
+        queryRun, card_img, url = duckduckgo.get_zci(lookupString, lang, True)
     except ValueError:
         if lang == 'de':
-            speech_output = "Es gab ein Problem beim erreichen von DackDackGo, versuche es später nochmal"
-            card_title = "Hallo Welt"
+            speech_output = "Es gab ein Problem beim erreichen von Dack Dack Go, versuche es später nochmal"
+            card_text = speech_output.replace( "Dack Dack Go", "DuckDuckGo")
         elif lang == 'en':
             speech_output = "There was a problem contacting DuckDuckGo, could you try a little later?"
-            card_title = "Hello World"
-        card_text = speech_output
-        reprompt_text = ""
-        should_end_session = True
+            card_text = speech_output
+
         return build_response({}, build_speechlet_response(
             card_title, speech_output, reprompt_text, should_end_session, card_text, None))
     else:
-        #withOut = re.sub(r"\(http\S+", "", queryRun, flags=re.MULTILINE)
-        withOut = re.sub(r"http\S+", "", queryRun, flags=re.MULTILINE)
-        speech_output = withOut
-        if lang == 'de':
-            speech_output = withOut + ", Ich schicke einen Link zu mehr Infos in die Alexa App."
-        elif lang == 'en':
-            speech_output = withOut + ", I've included a link for more info in the Alexa App."
-        card_title = myname + " - " + lookupString
-        reprompt_text = ""
-        should_end_session = True
-        card_text = queryRun.encode('utf-8')
+        queryRun = queryRun.encode('utf-8')
+        print("queryRun: " + queryRun)
+        print("url: " + url)
+        if queryRun.find('http') == 0:
+            speech_output = format_url(queryRun, lang, True)
+            card_text = queryRun
+            card_title = 'see this URL'
+            if lang == 'de':
+                card_title = 'Siehe hier'
+        elif queryRun == '-no-results-':
+            if not url:
+                speech_output = 'leider wurde nichts gefunden. Versuchs noch mal!'
+                reprompt_text = "Versuche eine andere Formulierung. Sage Stopp zum Beenden"
+                if lang == 'en':
+                    speech_output = 'nothing found. Try again!'
+                    reprompt_text = 'Formulate your question different. Say "stop" to leave'
+                should_end_session = False
+            else:
+                speech_output = url
+        else:
+            speech_output = queryRun + format_url(url, lang, False)
+
+        if card_img:
+            card_title = lookupString
+            card_text = queryRun
+            speech_output += br + 'Ich schicke eine Karte in die Alexa App.'
+
         return build_response({}, build_speechlet_response(
             card_title, speech_output, reprompt_text, should_end_session, card_text, card_img))
 
